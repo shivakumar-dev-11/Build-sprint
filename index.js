33bdc8ecb7a42f22eb1cc39acba45de1b457f0b0
Shivakumar <bashamonishivakumar081@gmail.com>
2026-08-14 09:41:57 +0530

    Add greeting functions to format names and generate greetings

---
+++ index.js
@@ -0,0 +1,22 @@
+function formatName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+function getGreeting(timeOfDay) {
+  if (timeOfDay === "morning") {
+    return "Good morning";
+  } else if (timeOfDay === "afternoon") {
+    return "Good afternoon";
+  } else {
+    return "Good evening";
+  }
+}
+
+function createGreeting(firstName, lastName, timeOfDay) {
+  let name = formatName(firstName, lastName);
+  let greeting = getGreeting(timeOfDay);
+
+  return greeting + ", " + name + "!";
+}
+
+console.log(createGreeting("Eswar", "Saketh", "morning"));
